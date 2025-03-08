7c978fbc1f848b3d6e65a6e865f2fa5999effb3b
Bijo Sebastian <bijosebastian757@gmail.com>
2025-03-08 15:29:43 +0530

    Update kinematics_helper_functions.py

--- Assignment3/kinematics_helper_functions.py
+++ Assignment3/kinematics_helper_functions.py
@@ -8,17 +8,19 @@ import numpy as np
 import math
 import robot_params
 
+def inv_kin(goal_position):
+    #Compute inverse kinematics function for 2 link manipulator
+    #Goal position given in m
+    #Return joint angles in radians
+
+    #Fill here
+    return [theta_1, theta_2]
+    
 def get_desired_joint_rate(joint_angles):
+    #Compute joint angle rate in rad/sec to achieve effector velocity along global x axis of 0.01m/s
     
-    #Compute 2x2 jacobian matrix for given joint angles [in radians] 
-    j11 = 
-    j12 = 
-    j21 = 
-    j22 = 
-    jacobian  = np.array([[j11, j12], [j21, j22]])
-    
-    #Compute desired joint angle rate by inverting jacobian and multiplying with desired end effector velocity
     desired_end_effector_velocity = np.array([[0.01], [0.0]])
     desired_joint_angle_rates = 
-    
+
+    #Fill here
     return [desired_joint_angle_rates[0], desired_joint_angle_rates[1]]
