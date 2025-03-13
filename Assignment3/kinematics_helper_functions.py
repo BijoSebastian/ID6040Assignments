0572170ac4fabff8dd264c483abb7c606feb6588
Bijo Sebastian <bijosebastian757@gmail.com>
2025-03-13 11:14:17 +0530

    Update kinematics_helper_functions.py

--- Assignment3/kinematics_helper_functions.py
+++ Assignment3/kinematics_helper_functions.py
@@ -7,14 +7,6 @@ Created on Tue Mar 28 09:51:21 2023
 import numpy as np
 import math
 import robot_params
-
-def inv_kin(goal_position):
-    #Compute inverse kinematics function for 2 link manipulator
-    #Goal position given in m
-    #Return joint angles in radians
-
-    #Fill here
-    return [theta_1, theta_2]
     
 def get_desired_joint_rate(joint_angles):
     #Compute joint angle rate in rad/sec to achieve effector velocity along global x axis of 0.01m/s
